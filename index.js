04bf95552e434a64f2b9c2217ae4f58141b3acf1
SVRourke <samuelvanrourke@gmail.com>
2021-08-18 10:01:01 -0400

    updated imports and usage of patterns

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const fs = require("fs");
 
 const FILE_PATH = "./test.md";
-
-const patterns = {
-  lines: /[\r\n$]+/,
-  anyHeading: /^\#+\s.*/,
-};
+const patterns = require("./lib/patterns");
 
 fs.readFile(FILE_PATH, "utf8", (err, data) => {
   if (err) console.log;
   const splitLines = data.split(patterns.lines);
 
   splitLines.forEach((line) => {
-    console.log(line.match(patterns.anyHeading));
+    console.log(!!line.match(patterns.headings.all));
   });
 
   console.log(splitLines.length);
