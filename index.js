2810c9fe87a5dba24ecedc891731a777a426ac56
SVRourke <samuelvanrourke@gmail.com>
2021-08-18 13:02:13 -0400

    remove empty lines from split lines, create function to validat heading and return the element object with heading type and content

--- index.js
+++ index.js
@@ -5,11 +5,33 @@ const patterns = require("./lib/patterns");
 
 fs.readFile(FILE_PATH, "utf8", (err, data) => {
   if (err) console.log;
-  const splitLines = data.split(patterns.lines);
+
+  // split file into lines
+  const splitLines = data.split(patterns.lines).filter((l) => l);
+
+  let paragraphCount = 0;
 
   splitLines.forEach((line) => {
-    console.log(!!line.match(patterns.headings.all));
+    // console.log(checkHeading(line), "\n");
+    // console.log(line, "\n", , "\n\n");
+    if (!!line.match(patterns.paragraph)) paragraphCount++;
+    console.log("LINE++ ", line, "\n");
   });
 
   console.log(splitLines.length);
 });
+
+const checkHeading = (line) => {
+  const validHeading = !!line.match(patterns.headings.all);
+
+  if (validHeading) {
+    const type = line.match(patterns.headings.type).length;
+
+    return {
+      type: `h${type}`,
+      content: line.slice(type + 1),
+    };
+  } else {
+    return false;
+  }
+};
