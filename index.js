10e755684203fa1964f696bc4bb06afdb8fcbd53
SVRourke <samuelvanrourke@gmail.com>
2021-08-23 15:23:12 -0400

    refactored broad classification, worked out subformatting, abstracted element wrapping into a utility function for use in replacements object

--- index.js
+++ index.js
@@ -9,43 +9,57 @@ fs.readFile(FILE_PATH, "utf8", (err, data) => {
 
   // split file into lines, remove empties
   const splitLines = stripEmpty(data.split(/([\r\n$]|<br>)+/));
-  const broadCategorized = [];
+  
+  // broadly categorize
+  const broad = splitLines.map((line) => {
+    return {
+      category: classifyElement(line),
+      element: line,
+    };
+  });
 
-  // broadly categorize the lines
-  while (splitLines.length > 0) {
-    const element = splitLines.shift();
-    broadCategorized.push({
-      category: classifyElement(element),
-      element: element,
-    });
-  }
+  // sub out format elements
+  const subbed = subformatting(broad);
 
-  // check for nested elements
+  // continue doing stuff
+  subbed.forEach((e) => console.log(e));
+});
 
-  // identify format elements
-  broadCategorized.forEach((e) => {
-    let { category, element } = e;
-    const formatElements = checkSubElements(element);
-    if (!!formatElements) {
-      formatElements.forEach((e) => {
+const subformatting = (arr) => {
+  const subbed = [];
+  arr.forEach((line) => {
+    let { category, element } = line;
+    const formats = checkSubElements(element);
+    if (!!formats) {
+      formats.forEach((e) => {
         element = replacements[e](element);
       });
     }
+    subbed.push({
+      category: category,
+      element: element,
+    });
   });
-});
+  return subbed;
+};
 
 // ---------------------  REPLACEMENT OPS ------------------------
+// accepts the text, outer and inner regex patterns and
+// the wrapper text in the format "<br>|</br>"
+const wrapElement = (line, outerPattern, innerPattern, wrapper) => {
+  const splitWrap = wrapper.split("|");
+  return line.replace(outerPattern, (a) => {
+    return `${splitWrap[0]}${a.replace(innerPattern, "")}${splitWrap[1]}`;
+  });
+};
+
 const replacements = {
   bold: (e) => {
-    return e.replace(/\*{2}[\w\s]+\*{2}/g, (a, b, c, d) => {
-      return `<strong>${a.replace(/\*{2}/g, "")}</strong>`;
-    });
+    return wrapElement(e, /\*{2}.+\*{2}/g, /\*{2}/g, "<strong>|</strong>");
   },
 
   italic: (e) => {
-    return = e.replace(/\_[\w\s]+\_/g, (a, b, c, d) => {
-      return `<em>${a.replace(/\_/g, "")}</em>`;
-    });
+    return wrapElement(e, /\_.+\_/g, /\_/g, "<em>|</em>");
   },
 
   quoteNestedUl: (e) => {
