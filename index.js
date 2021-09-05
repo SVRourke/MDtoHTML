5787a33fb543f0abf0b487c2f8188bbf70b17306
SVRourke <samuelvanrourke@gmail.com>
2021-09-05 16:41:53 -0400

    reworking combinatory logic

--- index.js
+++ index.js
@@ -7,11 +7,44 @@ const newDoc = [];
 
 for (const segment of parsedDoc) {
   const compound = segment.hasOwnProperty("subElements");
-  const { type, content } = segment;
+  const { type, content, subElements } = segment;
 
   let newElement;
 
   if (compound) {
+    for (const index in subElements) {
+      const { category, content } = subElements[index];
+
+      while (subElements.length > 1) {
+        const currentElement = subElements.shift();
+        const contents = [currentElement];
+        console.log(subElements.length);
+
+        if (currentElement.category === subElements[0]["category"]) {
+          console.log("match");
+        }
+      }
+
+      // FOR EACH SUB ELEMENT
+      // ADD CURRENT TO ACCRUED CONTENT ARRAY
+      // IF THE CURRENT ELEMENT MATCHES THE NEXT ELEMENT
+      // PUSH NEXT
+      // CONTINUE
+      // ELSE
+      // PROCESS AS INDIVIDUAL
+
+      // while (searching) {
+      //   const second = index + 1;
+      //   if (index > subElements.length) {
+      //     break;
+      //   }
+
+      //   if (category == subElements[second]["category"]) {
+      //     console.log("match");
+      //     second++;
+      //   } else break;
+      // }
+    }
     console.log("Compound", segment.subElements); // assign to newElement
   } else {
     newElement = wrappers[segment.type](segment.content); // assign to newElement
