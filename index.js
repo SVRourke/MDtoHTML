93ce95885cf4511b2ec3f74980c6d435c32eeb90
SVRourke <samuelvanrourke@gmail.com>
2021-09-06 08:13:03 -0400

    now trying to use reduce to combing consecutive elements of the same type

--- index.js
+++ index.js
@@ -9,45 +9,28 @@ for (const segment of parsedDoc) {
   const compound = segment.hasOwnProperty("subElements");
   const { type, content, subElements } = segment;
 
-  let newElement;
-
   if (compound) {
-    for (const index in subElements) {
-      const { category, content } = subElements[index];
+    const result = subElements.reduce(reducerfunction, []);
 
-      while (subElements.length > 1) {
-        const currentElement = subElements.shift();
-        const contents = [currentElement];
-        console.log(subElements.length);
-
-        if (currentElement.category === subElements[0]["category"]) {
-          console.log("match");
-        }
-      }
-
-      // FOR EACH SUB ELEMENT
-      // ADD CURRENT TO ACCRUED CONTENT ARRAY
-      // IF THE CURRENT ELEMENT MATCHES THE NEXT ELEMENT
-      // PUSH NEXT
-      // CONTINUE
-      // ELSE
-      // PROCESS AS INDIVIDUAL
-
-      // while (searching) {
-      //   const second = index + 1;
-      //   if (index > subElements.length) {
-      //     break;
-      //   }
-
-      //   if (category == subElements[second]["category"]) {
-      //     console.log("match");
-      //     second++;
-      //   } else break;
-      // }
-    }
-    console.log("Compound", segment.subElements); // assign to newElement
+    console.log("Compound"); // assign to newElement
   } else {
     newElement = wrappers[segment.type](segment.content); // assign to newElement
   }
   newDoc.push(newElement);
 }
+
+// start
+// 1 shift the first element
+// 2 add content to array,
+// 3 if the new first element is the same goto 1
+// 3.1 if not the same, push new array to newSubelements as obj with category
+// goto 1
+
+const result = activities.reduce((r, o) => {
+  const last = r[r.length - 1];
+  
+  if(last && last.type === o.type) last.duration += o.duration;
+  else r.push({ ...o });
+  
+  return r;
+}, []);
